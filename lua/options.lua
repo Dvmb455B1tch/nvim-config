c6f47e7c864dd4358b16ab6e99d4763c13f433e5
Ritsoniaina Arovony Ramanarivosoa <riramana@c1r3s8.42antananarivo.mg>
2025-09-22 09:02:37 +0300

    Looking great

--- lua/options.lua
+++ lua/options.lua
@@ -12,3 +12,29 @@ vim.cmd [[
   hi LineNr guibg=NONE ctermbg=NONE
   hi SignColumn guibg=NONE ctermbg=NONE
 ]]
+
+-- Terminal settings
+vim.o.shell = "fish"  -- Set fish as default shell
+vim.g.terminal_color_0 = "#45475a"
+vim.g.terminal_color_1 = "#f38ba8"
+vim.g.terminal_color_2 = "#a6e3a1"
+vim.g.terminal_color_3 = "#f9e2af"
+vim.g.terminal_color_4 = "#89b4fa"
+vim.g.terminal_color_5 = "#f5c2e7"
+vim.g.terminal_color_6 = "#94e2d5"
+vim.g.terminal_color_7 = "#bac2de"
+
+-- Terminal keybindings
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- Apply terminal keymaps when entering terminal
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,92 @@ return
 	},
 	config =  function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+		local pickers = require("telescope.pickers")
+		local finders = require("telescope.finders")
+		local conf = require("telescope.config").values
+		
+		-- Custom terminal picker
+		local function terminal_picker()
+			pickers.new({}, {
+				prompt_title = "Terminal Actions",
+				finder = finders.new_table({
+					results = {
+						{ "New Terminal", "new_terminal" },
+						{ "Horizontal Terminal", "horizontal_terminal" },
+						{ "Vertical Terminal", "vertical_terminal" },
+						{ "Floating Terminal", "floating_terminal" },
+						{ "Fish Shell (Float)", "fish_float" },
+						{ "Fish Shell (Horizontal)", "fish_horizontal" },
+					}
+				}),
+				sorter = conf.generic_sorter({}),
+				attach_mappings = function(prompt_bufnr, map)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection = action_state.get_selected_entry()
+						local cmd = selection[2]
+						
+						if cmd == "new_terminal" then
+							vim.cmd("ToggleTerm")
+						elseif cmd == "horizontal_terminal" then
+							vim.cmd("ToggleTerm direction=horizontal")
+						elseif cmd == "vertical_terminal" then
+							vim.cmd("ToggleTerm direction=vertical")
+						elseif cmd == "floating_terminal" then
+							vim.cmd("ToggleTerm direction=float")
+						elseif cmd == "fish_float" then
+							vim.cmd("ToggleTerm direction=float shell=fish")
+						elseif cmd == "fish_horizontal" then
+							vim.cmd("ToggleTerm direction=horizontal shell=fish")
+						end
+					end)
+					return true
+				end,
+			}):find()
+		end
+		
+		-- Enhanced file picker with terminal option
+		local function enhanced_find_files()
+			pickers.new({}, {
+				prompt_title = "Find Files & Actions (type 'terminal' for terminal options)",
+				finder = finders.new_oneshot_job({"find", ".", "-type", "f"}, {}),
+				sorter = conf.file_sorter({}),
+				attach_mappings = function(prompt_bufnr, map)
+					-- Default file opening
+					actions.select_default:replace(function()
+						local current_picker = action_state.get_current_picker(prompt_bufnr)
+						local prompt = current_picker:_get_prompt()
+						
+						-- Check if user typed "terminal"
+						if prompt:lower():match("terminal") then
+							actions.close(prompt_bufnr)
+							terminal_picker()
+							return
+						end
+						
+						-- Default file opening behavior
+						actions.file_edit(prompt_bufnr)
+					end)
+					
+					-- Add custom mapping for terminal
+					map('i', '<C-t>', function()
+						actions.close(prompt_bufnr)
+						terminal_picker()
+					end)
+					
+					return true
+				end,
+			}):find()
+		end
+		
+		-- Key mappings
+		vim.keymap.set('n', '<C-p>', enhanced_find_files, { desc = "Find Files (type 'terminal' for terminal options)" })
+		
+		-- Direct terminal access
+		vim.keymap.set('n', '<leader>tt', terminal_picker, { desc = "Terminal Picker" })
+		vim.keymap.set('n', '<leader>tf', function() vim.cmd("ToggleTerm direction=float shell=fish") end, { desc = "Float Fish Terminal" })
 		
 		-- Set up Ctrl+G for live grep (search text in all files)
 		vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = "Live Grep - Search in All Files" })
