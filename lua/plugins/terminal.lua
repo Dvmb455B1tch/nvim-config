93d5d6d8c94b0f9bbad7b0ad9cfea758d4fc8b77
Ritsoniaina Arovony Ramanarivosoa <riramana@c1r12s10.42antananarivo.mg>
2025-11-03 14:41:27 +0300

    a fix

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			require("toggleterm").setup({
 				size = 20,
-				open_mapping = [[<c-\>]],
+				open_mapping = [[<M-\>]], -- Changed from <c-\> to <M-\> (alt-\)
 				hide_numbers = true,
 				shade_filetypes = {},
 				shade_terminals = true,
@@ -30,9 +30,9 @@ return {
 			-- Terminal keybindings with proper window splitting support
 			function _G.set_terminal_keymaps()
 				local opts = {buffer = 0}
-				-- Exit terminal mode
-				vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-				vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+				-- Exit terminal mode - updated to use alt-\ consistently
+				vim.keymap.set('t', '<esc>', [[<M-\><C-n>]], opts)
+				vim.keymap.set('t', 'jk', [[<M-\><C-n>]], opts)
 				
 				-- Window navigation from terminal mode
 				vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
@@ -40,11 +40,11 @@ return {
 				vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
 				vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 				
-				-- Window operations from terminal mode (fix for splitting)
-				vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-				vim.keymap.set('t', '<C-w>s', [[<C-\><C-n><C-w>s]], opts)
-				vim.keymap.set('t', '<C-w>v', [[<C-\><C-n><C-w>v]], opts)
-				vim.keymap.set('t', '<C-w>n', [[<C-\><C-n><C-w>n]], opts)
+				-- Window operations from terminal mode (fix for splitting) - updated to use alt-\
+				vim.keymap.set('t', '<C-w>', [[<M-\><C-n><C-w>]], opts)
+				vim.keymap.set('t', '<C-w>s', [[<M-\><C-n><C-w>s]], opts)
+				vim.keymap.set('t', '<C-w>v', [[<M-\><C-n><C-w>v]], opts)
+				vim.keymap.set('t', '<C-w>n', [[<M-\><C-n><C-w>n]], opts)
 			end
 
 			-- Apply terminal keymaps when entering terminal
